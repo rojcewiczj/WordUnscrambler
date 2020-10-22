3f90dd2e16f18e406f0a592472af14f8f3cab0d2
John Rojcewicz <rojcewiczj@gmail.com>
2020-10-22 15:57:07 -0500

    Add project files.

---
+++ WordUnscrambler/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WordUnscrambler
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 10;
+            ChangeNumber(a);
+            Console.WriteLine(a);
+
+
+        }
+
+        static void ChangeNumber(int a)
+        {
+            a = 90;
+
+        }
+    }
+}
