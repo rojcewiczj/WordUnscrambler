aa1da2f743ab1358d9167e53081051909e11bcc8
John Rojcewicz <rojcewiczj@gmail.com>
2020-10-23 15:25:31 -0500

    working on text file reading/writing

--- WordUnscrambler/Program.cs
+++ WordUnscrambler/Program.cs
@@ -5,24 +5,34 @@ using System.Linq;
 using System.Net;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace WordUnscrambler
 {
     class Program
     {
+
         static void Main(string[] args)
         {
-            int a = 10;
-            ChangeNumber(a);
-            Console.WriteLine(a);
+
+            string[] lines = { "first line", "second Line", "this is the third line" };
+
+            File.WriteAllLines("MyfirstFile.txt", lines);
+
+            string[] fileContents = File.ReadAllLines("MyFirstFile.txt");
+
+            Console.WriteLine(fileContents);
+
+            foreach(string line in File.ReadLines("MyFirstFile.txt"))
+            {
+                Console.WriteLine(line);
+            }
+
+
 
 
         }
 
-        static void ChangeNumber(int a)
-        {
-            a = 90;
-
-        }
+      
     }
 }
